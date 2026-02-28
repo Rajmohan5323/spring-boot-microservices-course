11b52d0835b5e09c581fd7f4a14f992bd37effa6
Rajmohan <rajmohan5323@gmail.com>
2026-02-28 20:52:47 +0530

    Completed OrderAPI upto Unit and Integration test completed

---
+++ order-service/src/main/java/com/raj/bookstore/orders/domain/models/CreateOrderResponse.java
@@ -0,0 +1,3 @@
+package com.raj.bookstore.orders.domain.models;
+
+public record CreateOrderResponse(String orderNumber) {}
--- order-service/src/main/java/com/raj/bookstore/orders/web/controllers/OrderController.java
+++ order-service/src/main/java/com/raj/bookstore/orders/web/controllers/OrderController.java
@@ -1,8 +1,32 @@
 package com.raj.bookstore.orders.web.controllers;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.raj.bookstore.orders.domain.OrderService;
+import com.raj.bookstore.orders.domain.SecurityService;
+import com.raj.bookstore.orders.domain.models.CreateOrderRequest;
+import com.raj.bookstore.orders.domain.models.CreateOrderResponse;
+import jakarta.validation.Valid;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/orders")
-public class OrderController {}
+public class OrderController {
+    private static final Logger log = LoggerFactory.getLogger(OrderController.class);
+    private final OrderService orderService;
+    private final SecurityService securityService;
+
+    OrderController(OrderService orderService, SecurityService securityService) {
+        this.orderService = orderService;
+        this.securityService = securityService;
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    CreateOrderResponse createOrder(@Valid @RequestBody CreateOrderRequest request) {
+        String userName = securityService.getLoginUserName();
+        log.info("Creating order for user: {}", userName);
+        return orderService.createOrder(userName, request);
+    }
+}
