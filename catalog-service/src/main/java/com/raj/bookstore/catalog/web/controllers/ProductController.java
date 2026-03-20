f3d72d343035c2bc0a386ca0df83519943c7a957
Rajmohan <rajmohan5323@gmail.com>
2026-03-20 21:59:45 +0530

    Unite and Integration Test Completed

--- catalog-service/src/main/java/com/raj/bookstore/catalog/web/controllers/ProductController.java
+++ catalog-service/src/main/java/com/raj/bookstore/catalog/web/controllers/ProductController.java
@@ -1,16 +1,20 @@
 package com.raj.bookstore.catalog.web.controllers;
 
+import static org.hibernate.query.sqm.tree.SqmNode.log;
+
 import com.raj.bookstore.catalog.domain.PagedResult;
 import com.raj.bookstore.catalog.domain.Product;
 import com.raj.bookstore.catalog.domain.ProductNotFoundException;
 import com.raj.bookstore.catalog.domain.ProductService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/products")
 class ProductController {
-
+    private static final Logger log = LoggerFactory.getLogger(ProductController.class);
     private ProductService productService;
 
     ProductController(ProductService productService) {
@@ -19,7 +23,7 @@ class ProductController {
 
     @GetMapping
     PagedResult<Product> getProducts(@RequestParam(name = "page", defaultValue = "1") int pageNo) {
-        // log.info("Fetching products for page: {}", pageNo);
+        log.info("Fetching products for page: {}", pageNo);
         System.out.println("hellow");
         return productService.getProducts(pageNo);
     }
@@ -27,6 +31,7 @@ class ProductController {
     @GetMapping("/{code}")
     ResponseEntity<Product> getProductByCode(@PathVariable String code) {
         // sleap();
+        log.info("Fetching product for code: {}", code);
         return productService
                 .getProductByCode(code)
                 .map(ResponseEntity::ok)
